43a5843bdf48534dc30ffaf4b15501137f2be317
Genar Trias Ortiz <genar@acs.li>
2016-12-28 23:41:58 +0100

    starting to listen what is happening in consul server

--- app.js
+++ app.js
@@ -1,41 +1,35 @@
-'use strict';
-
-var config = require('config');
-var LE = require('letsencrypt');
-
-// Storage Backend
-var leStore = require('le-store-certbot').create({
-  configDir: '~/letsencrypt/etc',                          // or /etc/letsencrypt or wherever
-  debug: false
+var config           = require('config');
+var LE               = require('letsencrypt');
+var consulHost       = config.get('consul.host');
+var consul           = require('consul')({
+    host: consulHost
 });
 
-// ACME Challenge Handlers
-var leChallenge = require('le-challenge-fs').create({
-  webrootPath: '~/letsencrypt/var/',                       // or template string such as
-  debug: false                                            // '/srv/www/:hostname/.well-known/acme-challenge'
+// Getting consul agent nodename to start watcher
+consul.agent.self(function(err, result) {
+    if (err) return console.log(err);
+    var nodeName = result.Config.NodeName;
+    startWatcher(nodeName);
 });
 
-var le = LE.create({
-  server: LE.stagingServerUrl,                             // or LE.productionServerUrl
-  store: leStore,                                          // handles saving of config, accounts, and certificates
-  challenges: { 'http-01': leChallenge },                  // handles /.well-known/acme-challege keys and tokens
-  challengeType: 'http-01',                                // default to this challenge type
-  agreeToTerms: leAgree,                                   // hook to allow user to view and accept LE TOS
-  //, sni: require('le-sni-auto').create({})               // handles sni callback
-  //, debug: false
-  ////, log: function (debug) {console.log.apply(console, args);} // handles debug outputs
-});
+// Starting watcher
+function startWatcher(node) {
+    console.log('Starting watcher');
+    var watch = consul.watch({ method: consul.catalog.node.services, options: {'node': node}});
 
-var opts = {
-  domains: ['example.com'], email: 'user@email.com', agreeTos: true
-};
+    watch.on('change', function(data, res) {
+        requestCertificates(data);
+    });
 
-le.register(opts).then(function (certs) {
-  console.log(certs);
-  // privkey, cert, chain, expiresAt, issuedAt, subject, altnames
-}, function (err) {
-  console.error(err);
-});
+    watch.on('error', function(err) {
+        console.log('error:', err);
+    });
+}
+
+
+function requestCertificates(data) {
+  console.log(data);
+}
 
 var app = express();
 app.use('/', le.middleware());
