f5f376efe9495c85dde4f6ccc3c83701613e69db
Genar Trias Ortiz <genar@acs.li>
2016-12-29 11:05:12 +0100

    adding minimal example LE implementation

--- app.js
+++ app.js
@@ -6,6 +6,36 @@ var consul           = require('consul')({
     host: consulHost
 });
 
+// Storage Backend
+var leStore = require('le-store-certbot').create({
+  configDir: '~/letsencrypt/etc'                          // or /etc/letsencrypt or wherever
+, debug: false
+});
+
+
+// ACME Challenge Handlers
+var leChallenge = require('le-challenge-fs').create({
+  webrootPath: '~/letsencrypt/var/'                       // or template string such as
+, debug: false                                            // '/srv/www/:hostname/.well-known/acme-challenge'
+});
+
+
+function leAgree(opts, agreeCb) {
+  // opts = { email, domains, tosUrl }
+  agreeCb(null, opts.tosUrl);
+}
+
+le = LE.create({
+  server: LE.stagingServerUrl,                             // or LE.productionServerUrl
+  store: leStore,                                          // handles saving of config, accounts, and certificates
+  challenges: { 'http-01': leChallenge },                  // handles /.well-known/acme-challege keys and tokens
+  challengeType: 'http-01',                                // default to this challenge type
+  agreeToTerms: leAgree,                                   // hook to allow user to view and accept LE TOS
+//, sni: require('le-sni-auto').create({})                // handles sni callback
+  debug: false,
+//, log: function (debug) {console.log.apply(console, args);} // handles debug outputs
+});
+
 // Getting consul agent nodename to start watcher
 consul.agent.self(function(err, result) {
     if (err) return console.log(err);
