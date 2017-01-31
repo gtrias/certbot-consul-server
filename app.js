25ce669fdfdef725b6f75f665fcbf108a6ec4fdc
Genar <genar@acs.li>
2017-01-31 16:31:52 +0100

    retrieving all datacenter services to ask for a certificate

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var config           = require('config');
 var LE               = require('letsencrypt');
 var express          = require('express');
 var consulHost       = config.get('consul.host');
+var async =  require('async');
 var consul           = require('consul')({
     host: consulHost
 });
@@ -33,30 +34,57 @@ le = LE.create({
   challengeType: 'http-01',                                // default to this challenge type
   loopbackPort: config.get('letsencrypt.loopbackPort'),
   agreeToTerms: leAgree,                                   // hook to allow user to view and accept LE TOS
-//, sni: require('le-sni-auto').create({})                // handles sni callback
+//  sni: require('le-sni-auto').create({})                // handles sni callback
   debug: false,
-//, log: function (debug) {console.log.apply(console, args);} // handles debug outputs
+  log: function (debug) {
+    console.log(debug);
+  } // handles debug outputs
 });
 
 // Getting consul agent nodename to start watcher
-consul.agent.self(function(err, result) {
-    if (err) return console.log(err);
-    var nodeName = result.Config.NodeName;
-    startWatcher(nodeName);
-});
+function startListen() {
+  consul.agent.self(function(err, result) {
+    if (err) {
+      console.log(err);
+      startListen();
+    } else {
+      startWatcher(result);
+    }
+  });
+}
+// First execution
+startListen();
 
 // Starting watcher
 function startWatcher(node) {
-    console.log('Starting watcher');
-    var watch = consul.watch({ method: consul.catalog.node.services, options: {'node': node}});
+  var nodeName = node.Config.NodeName;
+  var watch = consul.watch({ method: consul.catalog.service.list, options: {'node': nodeName}});
 
-    watch.on('change', function(data, res) {
-        requestCertificates(data);
+  watch.on('change', function(data, res) {
+    var services = [];
+
+    async.forEachOf(data, function(service, key, callback) {
+      consul.catalog.service.nodes(key, function(err, result) {
+        if (err) throw err;
+
+        services.push({
+          ID: key,
+          nodes: result
+        })
+
+        callback();
+      });
+    }, function (err) {
+      if (err) return console.log(err);
+
+      requestCertificates(services);
     });
 
-    watch.on('error', function(err) {
-        console.log('error:', err);
-    });
+  });
+
+  watch.on('error', function(err) {
+    console.log('error:', err);
+  });
 }
 
 function requestCertificates(data) {
@@ -102,20 +130,18 @@ function requestCertificates(data) {
  * Convert services payload to key-value pairs with SSL_VIRTUAL_HOST and SSL_EMAIL
  */
 function extractDomainEmailPairs(data) {
-  var groupedServices = _.indexBy(data.Services, 'Service');
 
   // Hacky way to get object array
   var result = [];
 
-  for (var property in groupedServices) {
-      if (groupedServices.hasOwnProperty(property)) {
-          var value = groupedServices[property];
+data.forEach(function (element) {
+
+element.nodes.forEach(function (node) {
+
           var pair = [];
 
-          if (value.Tags) {
-
-              for (var j = 0; j < value.Tags.length; j++) {
-                  var kV = value.Tags[j].split('=');
+              for (var j = 0; j < node.ServiceTags.length; j++) {
+                  var kV = node.ServiceTags[j].split('=');
                   if (kV[0] && kV[0] === 'SSL_VIRTUAL_HOST'){
                       pair['SSL_VIRTUAL_HOST'] = kV[1];
                   }
@@ -127,10 +153,11 @@ function extractDomainEmailPairs(data) {
               if (pair['SSL_VIRTUAL_HOST'] && pair['SSL_EMAIL']) {
                   result.push(pair);
               }
-          }
 
-      }
-  }
+
+});
+
+});
 
   return result;
 }
